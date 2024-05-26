733f80d73b2dcc5492c3c159e59c1848cf6fae32
woghwo <aji092712@gmail.com>
2024-05-26 12:32:15 +0900

    우선순위 mysql 연동 후 다시 수정

--- src/main/java/hello/hellospring/service/PriorityServiceImpl.java
+++ src/main/java/hello/hellospring/service/PriorityServiceImpl.java
@@ -7,6 +7,7 @@ import hello.hellospring.repository.PriorityRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 @Service
 @Transactional
@@ -21,9 +22,19 @@ public class PriorityServiceImpl implements PriorityService {
     @Override
     public ResponseVo insertPriority(PriorityVo priorityVo) {
         try {
-            PriorityEntity priorityEntity = new PriorityEntity();
-            priorityEntity.setPriorityNm(priorityVo.getPriorityNm());
-            priorityEntity.setLevel(priorityVo.getLevel());
+            if(ObjectUtils.isEmpty(priorityVo)) {
+                return null;
+            }
+            PriorityEntity chkPriorityEntity = priorityRepository.findByPriorityNm(priorityVo.getPriorityNm());
+            /** 우선순위 중복체크 */
+            if(chkPriorityEntity != null) {
+                return new ResponseVo(11,"이미 존재하는 우선순위입니다.1");
+            }
+            /** 우선순위 추가 */
+            PriorityEntity priorityEntity = PriorityEntity.builder()
+                    .priorityNm(priorityVo.getPriorityNm())
+                    .level(priorityVo.getLevel())
+                    .build();
             priorityRepository.save(priorityEntity);
             return new ResponseVo(200,"SUCCESS");
         } catch (Exception e) {
@@ -34,9 +45,15 @@ public class PriorityServiceImpl implements PriorityService {
     @Override
     public ResponseVo updatePriority(PriorityVo priorityVo) {
         try {
+            if(ObjectUtils.isEmpty(priorityVo)) {
+                return null;
+            }
             /** 받은 파라미터의 레벨로 우선순위 데이터 조회 => 기본키 알기 위한 작업 */
             PriorityEntity priorityEntity = priorityRepository.findByPriorityNm(priorityVo.getPriorityNm());
-            /** 위 entity를 통해 업데이트 해주기 */
+            if(priorityEntity == null) {
+                return new ResponseVo(11, "우선순위가 존재하지 않습니다.");
+            }
+            /** 위 우선순위 entity를 통해 업데이트 해주기 */
             PriorityEntity updatePriorityEntity = PriorityEntity.builder()
                     .priorityId(priorityEntity.getPriorityId())
                     .priorityNm(priorityVo.getPriorityNm())
