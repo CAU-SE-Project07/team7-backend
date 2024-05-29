a99e73c2366b09c63fec8f6f629fedb642cb2853
hojin.lee <winghove@gmail.com>
2024-05-29 09:50:00 +0900

    slight change

--- src/main/java/hello/hellospring/Vo/CommentVo.java
+++ src/main/java/hello/hellospring/Vo/CommentVo.java
@@ -11,6 +11,6 @@ public class CommentVo {
     private String content;
     private String date;
     private String userId; // 현재 로그인한 사용자 아이디
-    private String title; // 이슈 제목(고유값)
+    private String issueTitle; // 이슈 제목(고유값)
     private String projectNm; // 프로젝트명(고유값)
 }
